4912cdb90dcd6f683851e6567f9edbbeb86e377d
Nick Rau <nickrau3@gmail.com>
2020-06-30 11:09:12 +0200

    Removed unnecessary repository

--- src/main/java/com/example/bedsysteem/data/repository/BedListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bedsysteem.data.repository;
-
-import com.example.bedsysteem.data.entity.BedList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BedListRepository extends JpaRepository<BedList, String> {
-}
